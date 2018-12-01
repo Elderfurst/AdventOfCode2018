d9c89c4d1bd1e5890878dcf9f248b570b4e12a99
Nick Anderson <njcanderson@gmail.com>
2018-12-01 08:27:49 -0500

    Day 1 solution to both parts

---
+++ AdventOfCode2018/Day1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2018
+{
+    public class Day1
+    {
+        private static readonly int[] _inputs = File.ReadAllText(@"Inputs/Day1.txt").Split('\n').Select(int.Parse).ToArray();
+        public void Run()
+        {
+            PartOne();
+            PartTwo();
+        }
+
+        private void PartOne()
+        {
+            var frequency = 0;
+            foreach (var input in _inputs)
+            {
+                frequency += input;
+            }
+
+            Console.WriteLine(frequency);
+        }
+
+        private void PartTwo()
+        {
+            var frequency = 0;
+            var seenFrequencies = new List<int>();
+            var inputCounter = 0;
+
+            while (!seenFrequencies.Contains(frequency))
+            {
+                seenFrequencies.Add(frequency);
+                frequency += _inputs[inputCounter % _inputs.Length];
+
+                inputCounter++;
+            }
+
+            Console.WriteLine(frequency);
+        }
+    }
+}
---
+++ AdventOfCode2018/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AdventOfCode2018
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var run = new Day1();
+            run.Run();
+        }
+    }
+}
